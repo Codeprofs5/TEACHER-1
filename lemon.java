e3746ab908243b5af17c8a359b8ab6d319e57683
Codeprofs5 <164242985+Codeprofs5@users.noreply.github.com>
2024-04-14 20:47:28 +0530

    Update lemon.java

--- lemon.java
+++ lemon.java
@@ -1,27 +1,66 @@
-=
-//swap using multiplication and division,instance variable and formal parameters shouldnot be same
-class swapm
+import java.awt.*;
+import java.awt.event.*;
+public class Mychoice extends Frame implements ItemListener
 {
-int a,b;
-void swap(int i,int j)
-{
-if(j>0)
-{
-i=i*j;
-j=i/j;
-i=i/j;
-System.out.println("after swaping"+i+" "+j);
+	private Label LbL;
+	private Label LbL1;
+	private TextField txt;
+	private Button btn;
+	String msg;
+	Choice ch1;
+	Font F = new Font ("Forte",Font.PLAIN,18);
+	Mychoice()
+	{
+		setLayout(new FlowLayout());
+		LbL=new Label("Enter item:-");
+		LbL.setFont(F);
+		add(LbL);
+		txt=new TextField(20);
+		txt.setFont(F);
+		add(txt);
+		LbL1=new Label("series this item:-");
+		LbL1.setFont(F);
+		add(LbL1);
+		ch1=new Choice();
+		ch1.add("coffee");
+		ch1.add("tea");
+		ch1.add("ice cream");
+		add(ch1);
+		ch1.addItemListener(this);
+		btn=new Button("ADD");
+		btn.setFont(F);
+		add(btn);
+		btn.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				System.exit(0);
+			}
+		
+		});
+		setTitle("My choice box");
+		setSize(300,300);
+		setVisible(true);
+	}
+	public void itemstatechanged(ItemEvent e)
+	{
+		repaint();
+	}
+	public void paint(Graphics g)
+	{
+		Font StringFont = new Font("sanSerif",Font.PLAIN,18);
+		g.setFont(StringFont);
+		g.drawString("selectedItem:-",20,150);
+		msg=ch1.getSelectedItem();
+		g.drawString(msg,150,150);
+	}
+	public static void main(String args[])
+	{
+		Mychoice cs= new Mychoice();
+	}
 }
-else
-System.out.println("invalid");
-}
-public static void main(String args[])
-{
-int num1=20;
-int num2=30;
-System.out.println("before swaping"+num1+" "+num2);
-swapm obj =new swapm();
-obj.swap(num1,num2);
 
-}
-}
+			
+	
+			
+
