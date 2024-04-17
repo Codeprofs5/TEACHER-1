f236bf818eb901e1ea1dd95a9fcc171597fe3a1a
Codeprofs5 <164242985+Codeprofs5@users.noreply.github.com>
2024-04-17 15:15:03 +0530

    Add JAVACODE/2001.java

---
+++ JAVACODE/2001.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int []a=new int[n];
+for(int i=0;i<n;i++)
+{
+    a[i]=scan.nextInt();
+}
+
+for(int k=0;k<a.length;k++)
+{
+    System.out.println(a[k]);
+}
+	}
+}
+
+
+
+        
+
+       
